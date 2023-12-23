9d071646de6b829b6e9ac2d9ffc7c5c77a3769ad
James Cox-Morton <james.cox-morton@arm.com>
2023-12-23 09:32:03 +0000

    21

---
+++ 21-nth.clj
@@ -0,0 +1,13 @@
+(defn nther
+  [s n]
+  (cond
+    (= n 0) (first s)
+    :else (recur (rest s) (- n 1))))
+
+
+((defn tests
+   []
+   (and
+     (= (nther [] 1) nil)
+     (= (nther [1] 0) 1)
+     (= (nther [1 2] 1) 2))))
