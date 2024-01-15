bcce2c8bdf9cc558c6d5d454e337ea7ba94df5bb
James Cox-Morton <james.cox-morton@arm.com>
2024-01-15 09:14:58 +0000

    Solve 1 indexed

--- 21-nth.clj
+++ 21-nth.clj
@@ -5,9 +5,10 @@
     :else (recur (rest s) (- n 1))))
 
 
-((defn tests
-   []
-   (and
-     (= (nther [] 1) nil)
-     (= (nther [1] 0) 1)
-     (= (nther [1 2] 1) 2))))
+
+
+
+(and
+  (= (nther [] 1) nil)
+  (= (nther [1] 0) 1)
+  (= (nther [1 2] 1) 2))
