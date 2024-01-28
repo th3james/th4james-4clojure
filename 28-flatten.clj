b8aa74554cade1b0033d819d2d0c0990ce1b6042
James Cox-Morton <james.cox-morton@arm.com>
2024-01-28 16:28:00 +0000

    WIP eager version of flattr

--- 28-flatten.clj
+++ 28-flatten.clj
@@ -7,11 +7,27 @@
         (cons (first s) (flattr (rest s)))))))
 
 
-(take 3 (flattr '()))
-(take 3 (flattr '(1)))
-(take 3 (flattr '((1))))
-(take 3 (flattr '((1) 2)))
-(take 3 (flattr '((1 2) 3)))
-(take 5 (flattr '((1 2) (3 4))))
-(take 5 (flattr '(1 2 (3 4))))
+(defn eager-flattr
+  [v]
+  (loop [r [] v v]
+    (cond
+      (sequential? (first v)) (recur r (concat (first v) (rest v)))
+      (empty? v) r
+      :else (recur (conj (first v) r) (rest v)))))
 
+
+(defn tests
+  [flatfn]
+  (and
+    (= (take 3 (flatfn '())) '())
+    (= (take 3 (flatfn '(1))) '(1))
+    (= (take 3 (flatfn '((1)))) '(1))
+    (= (take 3 (flatfn '((1) 2))) '(1 2))
+    (= (take 3 (flatfn '((1 2) 3))) '(1 2 3))
+    (= (take 5 (flatfn '((1 2) (3 4)))) '(1 2 3 4))
+    (= (take 5 (flatfn '((1 (2 3)) (4)))) '(1 2 3 4))
+    (= (take 5 (flatfn '(1 2 (3 4)))) '(1 2 3 4))))
+
+
+(tests flattr)
+(tests eager-flattr)
